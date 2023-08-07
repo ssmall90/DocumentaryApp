bece08e0657e9acabdfd09fcae6df0b0df22e26a
ssmall90 <ssmall9595@icloud.com>
2023-08-07 12:31:52 +0100

    Created movie model

--- DocumentaryAppLibrary/Models/DocumentaryMovieModel.cs
+++ DocumentaryAppLibrary/Models/DocumentaryMovieModel.cs
@@ -2,4 +2,15 @@
 
  public class DocumentaryMovieModel
  {
- }
+   [BsonId]
+   [BsonRepresentation(BsonType.ObjectId)]
+   public string Id { get; set; }
+   public string MovieName { get; set; }
+   public string MovieYear { get; set; }
+   public  string MovieRunTime { get; set; }
+   public int MovieRating { get; set; }
+   public  string MovieDescription { get; set; }
+   public CategoryModel MovieCategory { get; set; }
+   public bool HasUserWatched { get; set; } = false;
+
+}
