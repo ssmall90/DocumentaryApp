8ac97f8a58d382e2cd3fc0d5acc2a6224c5a76de
ssmall90 <ssmall9595@icloud.com>
2023-08-07 15:09:44 +0100

    Created documentary data access class

---
+++ DocumentaryAppLibrary/DataAccess/IMongoDocumentaryMovieData.cs
@@ -0,0 +1,9 @@
+﻿namespace DocumentaryAppLibrary.DataAccess;
+
+public interface IMongoDocumentaryMovieData
+{
+   Task<List<DocumentaryMovieModel>> GetAllDocumentarires();
+   Task<DocumentaryMovieModel> GetDocumentary(string id);
+   Task<List<DocumentaryMovieModel>> GetThreeDocumentariesByCategory(CategoryModel category);
+   Task UpdateDocumentary(DocumentaryMovieModel documentary);
+}
