37876bcb96b20e08550043eb4065c7caa6040c48
ssmall90 <ssmall9595@icloud.com>
2023-08-07 12:44:07 +0100

    Created basic movie model

---
+++ DocumentaryAppLibrary/Models/BasicDocumentaryMovieModel.cs
@@ -0,0 +1,18 @@
+﻿namespace DocumentaryAppLibrary.Models;
+public class BasicDocumentaryMovieModel
+{
+   [BsonRepresentation(BsonType.ObjectId)]
+   public string Id { get; set; }
+   public string MovieName { get; set; }
+
+   public BasicDocumentaryMovieModel()
+   {
+      
+   }
+
+   public BasicDocumentaryMovieModel(DocumentaryMovieModel movie)
+   {
+      Id = movie.Id;
+      MovieName = movie.MovieName;
+   }
+}
