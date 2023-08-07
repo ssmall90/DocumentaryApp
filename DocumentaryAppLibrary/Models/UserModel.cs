1468d116c92e4bdbee9235b3fd80313271585ba3
ssmall90 <ssmall9595@icloud.com>
2023-08-07 12:43:56 +0100

    Created user model

---
+++ DocumentaryAppLibrary/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DocumentaryAppLibrary.Models;
+public class UserModel
+{
+   [BsonId]
+   [BsonRepresentation(BsonType.ObjectId)]
+   public string Id { get; set; }
+   public string ObjectIdentifier { get; set; }
+   public string FirstName { get; set; }
+   public string LastName { get; set; }
+   public string DisplayName { get; set; }
+   public string EmailAddress { get; set; }
+   public List<BasicDocumentaryMovieModel> WatchedMovies { get; set; } = new();
+}
