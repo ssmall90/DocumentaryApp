d404337bd2b9746dcf83c36fb741e1124df7264f
ssmall90 <ssmall9595@icloud.com>
2023-08-08 14:52:35 +0100

    Created class for handling API requests

---
+++ DocumentaryAppLibrary/DataAccess/ITmdbService.cs
@@ -0,0 +1,4 @@
+﻿public interface ITmdbService
+{
+   Task<MovieResponse> GetPopularMoviesAsync();
+}
---
+++ DocumentaryAppLibrary/DataAccess/TmdbService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+public class TmdbService : ITmdbService
+{
+   private readonly HttpClient _httpClient;
+   private const string ApiKey = "8ad7b69946f794a837063ddaca7740d1";
+   Random randomizer = new Random();
+
+   public TmdbService(HttpClient httpClient)
+   {
+      _httpClient = httpClient;
+   }
+
+   public async Task<MovieResponse> GetPopularMoviesAsync()
+   {
+      int pageNumber = randomizer.Next(1, 500);
+      string apiUrl = $"https://api.themoviedb.org/3/discover/movie?api_key={ApiKey}&with_genres=99&page={pageNumber}";
+
+      HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+
+      if (response.IsSuccessStatusCode)
+      {
+         
+         string jsonResponse = await response.Content.ReadAsStringAsync();
+
+         Console.WriteLine(jsonResponse);
+         MovieResponse returnedValues = JsonConvert.DeserializeObject<MovieResponse>(jsonResponse);
+         return returnedValues.FilterResults(returnedValues.Results);
+
+      }
+      else
+      {
+         throw new Exception($"API request failed with status code: {response.StatusCode}");
+      }
+   }
+}
+
+
+
