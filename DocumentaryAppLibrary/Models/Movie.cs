78a8a744b854a66234487239216479c67818bc07
ssmall90 <ssmall9595@icloud.com>
2023-08-08 14:54:09 +0100

    Created classes for capturing deserialized JSON data

---
+++ DocumentaryAppLibrary/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
+
+namespace DocumentaryAppLibrary.Models;
+public class Movie
+{
+   public string id { get; set; }
+   public string title { get; set; }
+   public string release_date { get; set; }
+   public string overview { get; set; }
+   public float vote_average { get; set; }
+   public string poster_path { get; set; }
+   public string FullPosterPath
+   {
+      get
+      {
+         const string baseUrl = "https://image.tmdb.org/t/p/w500"; // You can choose different sizes (w92, w185, w500, etc.)
+         return $"{baseUrl}{poster_path}";
+      }
+   }
+
+}
---
+++ DocumentaryAppLibrary/Models/MovieResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace DocumentaryAppLibrary.Models;
+public class MovieResponse
+{
+   public List<Movie> Results { get; set; }
+
+
+   public MovieResponse FilterResults(List<Movie> movies)
+   {
+      Random random = new Random();
+      MovieResponse response = new MovieResponse();
+      response.Results = movies.OrderBy(x => random.Next(1, movies.Count)).Take(3).ToList();
+      return response;
+
+   }
+}
